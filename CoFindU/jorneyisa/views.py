ddefd42e6f3c6240c3c02771f4f77434cef1cf40
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-27 18:32:32 +0200

    add map in site

--- CoFindU/jorneyisa/views.py
+++ CoFindU/jorneyisa/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+import folium
 
 def general(request):
+    m = folium.Map()
+    m = m._repr_html_()
+    context = {
+        'title': 'Головна сторінка',
+        'map': m
+    }
     return render(
         request,
-        'jorneyisa/index.html',
-        {
-            'title': 'Головна сторінка',
-        }
+        'jorneyisa/map.html',
+        context
     )
