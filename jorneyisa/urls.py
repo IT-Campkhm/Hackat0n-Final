1d7790f29739beb98417b71f9c9342b161f343d2
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-28 00:57:13 +0200

    update

--- jorneyisa/urls.py
+++ jorneyisa/urls.py
@@ -5,5 +5,5 @@ from .views import *
 
 urlpatterns = [
     path('', general, name = 'home'),
-    path('tabel/', showtable, name = 'tabel'),
+    path('tabel/', showtable, name = 'tabel_event'),
 ]
