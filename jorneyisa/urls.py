84b19f17c489728f319cb446e8450c6604a8ea60
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-28 00:55:03 +0200

    update

--- jorneyisa/urls.py
+++ jorneyisa/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('map/', general, name = 'home'),
+    path('', general, name = 'home'),
     path('tabel/', showtable, name = 'tabel'),
 ]
