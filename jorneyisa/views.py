fa54930d72e7cd4dffbfa117e8f20bfcaa61f1d5
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-28 10:06:26 +0200

    гзвфеу

--- jorneyisa/views.py
+++ jorneyisa/views.py
@@ -6,11 +6,11 @@ import geocoder
 
 
 def general(request):
-
+    
     ukrain = geocoder.osm('UA')
-
-    m = folium.Map(location=[ukrain.latlng[0], ukrain.latlng[1]], zoom_start=7)
-
+    
+    m = folium.Map(location = [ukrain.latlng[0], ukrain.latlng[1]], zoom_start = 7)
+    
     kiev = geocoder.osm('Kiev')
     kharkiv = geocoder.osm('Kharkiv')
     odessa = geocoder.osm('Odessa')
@@ -21,18 +21,68 @@ def general(request):
     vinnitsa = geocoder.osm('Vinnitsa')
     zaporizhzhia = geocoder.osm('Zaporizhzhia')
     сhernihiv = geocoder.osm('Chernihiv')
-
-    citys = [kiev, kharkiv, odessa, lviv, zhytomyr, khmelnytskyi,
-             ivano_frankivsk, vinnitsa, zaporizhzhia, сhernihiv]
-
-    for city in range(len(citys)):
-        events = Events.objects.get(pk = city)
-        print(events)
-        folium.Marker(
-            [citys[city].latlng[0], citys[city].latlng[1]],
-            popup=f'{events}',
-            tooltip='Дізнатися більше про івент.'
-        ).add_to(m)
+  
+    folium.Marker(
+        [kiev.latlng[0], kiev.latlng[1]],
+        popup = '',
+        tooltip = 'Дізнатися більше про івент.'
+    ).add_to(m)
+    
+    folium.Marker(
+        [kharkiv.latlng[0], kharkiv.latlng[1]],
+        popup = '',
+        tooltip = 'Дізнатися більше про івент.'
+    ).add_to(m)
+    
+    folium.Marker(
+        [odessa.latlng[0], odessa.latlng[1]],
+        popup = '',
+        tooltip = 'Дізнатися більше про івент.'
+    ).add_to(m)
+    
+    folium.Marker(
+        [lviv.latlng[0], lviv.latlng[1]],
+        popup = '',
+        tooltip = 'Дізнатися більше про івент.'
+    ).add_to(m)
+    
+    folium.Marker(
+        [zhytomyr.latlng[0], zhytomyr.latlng[1]],
+        popup = '',
+        tooltip = 'Дізнатися більше про івент.'
+    ).add_to(m)
+    
+    folium.Marker(
+        [khmelnytskyi.latlng[0], khmelnytskyi.latlng[1]],
+        popup = '',
+        tooltip = 'Дізнатися більше про івент.'
+    ).add_to(m)
+    
+    folium.Marker(
+        [ivano_frankivsk.latlng[0], ivano_frankivsk.latlng[1]],
+        popup = '',
+        tooltip = 'Дізнатися більше про івент.'
+    ).add_to(m)
+    
+    folium.Marker(
+        [vinnitsa.latlng[0], vinnitsa.latlng[1]],
+        popup = '',
+        tooltip = 'Дізнатися більше про івент.'
+    ).add_to(m)
+    
+    folium.Marker(
+        [zaporizhzhia.latlng[0], zaporizhzhia.latlng[1]],
+        popup = '',
+        tooltip = 'Дізнатися більше про івент.'
+    ).add_to(m)
+    
+    
+    folium.Marker(
+        [сhernihiv.latlng[0], сhernihiv.latlng[1]],
+        popup = '',
+        tooltip = 'Дізнатися більше про івент.'
+    ).add_to(m)
+    
 
     m = m._repr_html_()
 
@@ -40,7 +90,7 @@ def general(request):
         'title': 'Знайди івент',
         'map': m,
     }
-
+    
     return render(
         request,
         'jorneyisa/map.html',
