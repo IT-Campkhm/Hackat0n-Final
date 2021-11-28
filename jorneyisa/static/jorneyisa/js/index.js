701eeaf7abc61c368b6df2e51a88f05b4b8192d8
fisvif <shutkovadim@mail.ru>
2021-11-28 05:06:11 +0200

    whitout-adaptive

---
+++ jorneyisa/static/jorneyisa/js/index.js
@@ -0,0 +1,19 @@
+let filter = document.querySelector(".filter"),
+    openFilter = document.querySelector(".open-filter");
+    closeFilter = document.querySelector(".close-filter");
+    blockMap = document.querySelector(".block-map");
+
+openFilter.onclick = showFilter;
+closeFilter.onclick = hideFilter;
+blockMap.onclick = showMap;
+
+
+function showFilter(){
+    filter.style.display = "flex";
+}
+function hideFilter(){
+    filter.style.display = "none";   
+}
+function showMap(){
+    blockMap.style.display = "none";   
+}
