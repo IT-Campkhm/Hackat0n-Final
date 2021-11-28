c623b03b858aab5732910615aa4c8118278c4093
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-28 05:03:09 +0200

    update

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release1: python manage.py migrate
+release: python manage.py migrate
 web: gunicorn CoFindU.wsgi --log-file -
--- jorneyisa/models.py
+++ jorneyisa/models.py
@@ -6,8 +6,6 @@ class Events(models.Model):
     description = models.TextField()
     cout_people = models.BigIntegerField()
     place = models.TextField()
-    type = models.TextField()
-    is_public = models.BooleanField(default = True)
     
     def __str__(self):
         return self.title
