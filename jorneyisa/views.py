57ee3078a5a2627d3cd658081d586d8e44a8305c
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-28 09:45:56 +0200

    update

--- jorneyisa/views.py
+++ jorneyisa/views.py
@@ -27,9 +27,10 @@ def general(request):
 
     for city in range(len(citys)):
         events = Events.objects.filter(pk = city)
+        print(events[0])
         folium.Marker(
             [citys[city].latlng[0], citys[city].latlng[1]],
-            popup=f'{events.title}',
+            popup=f'{events}',
             tooltip='Дізнатися більше про івент.'
         ).add_to(m)
 
