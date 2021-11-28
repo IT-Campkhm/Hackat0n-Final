4c531ada24ee4b4e9785824d81e387462b4ef996
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-28 11:45:12 +0200

    set debug false

--- CoFindU/settings.py
+++ CoFindU/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$3j6-x=zd2jgln)_!@d7+ys35ld^kruv*5n3fu65^e7115tjl2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
