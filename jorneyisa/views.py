72a393d89b8ddce65b6c75459b6293301dd29845
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-28 06:59:05 +0200

    update map point

--- jorneyisa/views.py
+++ jorneyisa/views.py
@@ -5,8 +5,34 @@ import geocoder
 
 
 def general(request):
-    g = geocoder.osm('UA')
-    m = folium.Map(location = [g.latlng[0], g.latlng[1]], zoom_start = 7)
+    
+    ukrain = geocoder.osm('UA')
+    
+    m = folium.Map(location = [ukrain.latlng[0], ukrain.latlng[1]], zoom_start = 7)
+    
+    kiev = geocoder.osm('UA', 'Kiev')
+    kharkiv = geocoder.osm('UA', 'Kharkiv')
+    odessa = geocoder.osm('UA', 'Odessa')
+    lviv = geocoder.osm('UA', 'Lviv')
+    
+    
+    folium.Marker(
+        [kiev.latlng[0], kiev.latlng[1]]
+    ).add_to(m)
+    
+    folium.Marker(
+        [kharkiv.latlng[0], kharkiv.latlng[1]]
+    ).add_to(m)
+    
+    folium.Marker(
+        [odessa.latlng[0], odessa.latlng[1]]
+    ).add_to(m)
+    
+    folium.Marker(
+        [lviv.latlng[0], lviv.latlng[1]]
+    ).add_to(m)
+    
+    
     m = m._repr_html_()
 
     context = {
