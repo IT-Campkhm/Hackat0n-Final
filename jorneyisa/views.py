1ceb4dbdf8c26d01293671fd5cdc630da125f79f
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-28 09:34:24 +0200

    update

--- jorneyisa/views.py
+++ jorneyisa/views.py
@@ -6,11 +6,11 @@ import geocoder
 
 
 def general(request):
-    
+
     ukrain = geocoder.osm('UA')
-    
-    m = folium.Map(location = [ukrain.latlng[0], ukrain.latlng[1]], zoom_start = 7)
-    
+
+    m = folium.Map(location=[ukrain.latlng[0], ukrain.latlng[1]], zoom_start=7)
+
     kiev = geocoder.osm('Kiev')
     kharkiv = geocoder.osm('Kharkiv')
     odessa = geocoder.osm('Odessa')
@@ -21,25 +21,25 @@ def general(request):
     vinnitsa = geocoder.osm('Vinnitsa')
     zaporizhzhia = geocoder.osm('Zaporizhzhia')
     сhernihiv = geocoder.osm('Chernihiv')
-    
-    citys = [kiev, kharkiv, odessa, lviv, zhytomyr, khmelnytskyi, ivano_frankivsk, vinnitsa, zaporizhzhia, сhernihiv]
+
+    citys = [kiev, kharkiv, odessa, lviv, zhytomyr, khmelnytskyi,
+             ivano_frankivsk, vinnitsa, zaporizhzhia, сhernihiv]
     events = Events.objects.all()
-    
-    for city in range(len(citys)):
-        for e in events:
-            folium.Marker(
-                [citys[city].latlng[0], citys[city].latlng[1]],
-                popup = f'{e.title}',
-                tooltip = 'Дізнатися більше про івент.'
-            ).add_to(m)
-    
+
+    for city, event in citys, events:
+        folium.Marker(
+            [citys[city].latlng[0], citys[city].latlng[1]],
+            popup=f'{event.title}',
+            tooltip='Дізнатися більше про івент.'
+        ).add_to(m)
+
     m = m._repr_html_()
 
     context = {
         'title': 'Знайди івент',
         'map': m,
     }
-    
+
     return render(
         request,
         'jorneyisa/map.html',
